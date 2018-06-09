8ee074768c1dffe6132ac61c743765cc44092a81
errorr404 <dkbishwash@gmail.com>
2018-06-10 03:02:55 +0530

    value passed direct to handler function from the onclick

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './memory_game_my_code';
+//import App from './main_code';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/memory_game_my_code.js
+++ src/memory_game_my_code.js
@@ -12,11 +12,11 @@ export default class App extends React.Component {
      currNum: 0
    };
   }
- handleUpdate(val){
+ handleUpdate = (val)=>{
   //alert('asigh');
 
    val = parseInt(val,10);
-  // alert(val);
+  //alert(val);
   // console.log('val'+val);
  var curScore = parseInt(this.state.score,10);
   //console.log('curScore'+curScore);
@@ -59,72 +59,81 @@ export default class App extends React.Component {
 
 
   class PressButtons extends React.Component {
-    constructor(props) {
-      super(props);
-      this.clickOne = this.clickOne.bind(this);
-      this.clickTwo = this.clickTwo.bind(this);
-      this.clickThree = this.clickThree.bind(this);
-      this.clickFour = this.clickFour.bind(this);
-      this.clickFive = this.clickFive.bind(this);
-      this.clickSix = this.clickSix.bind(this);
-      this.clickSeven = this.clickSeven.bind(this);
-      this.clickEight = this.clickEight.bind(this);
-      this.clickNine = this.clickNine.bind(this);
-    }
-    clickOne(e){
-         e.preventDefault();
-         this.props.handleUpdate(1);
-    }
-    clickTwo(e){
-         e.preventDefault();
-         this.props.handleUpdate(2);
-    }
-    clickThree(e){
-         e.preventDefault();
-         this.props.handleUpdate(3);
-    }
-    clickFour(e){
-         e.preventDefault();
-         this.props.handleUpdate(4);
-    }
-    clickFive(e){
-         e.preventDefault();
-         this.props.handleUpdate(5);
-    }
-    clickSix(e){
-         e.preventDefault();
-         this.props.handleUpdate(6);
-    }
-    clickSeven(e){
-         e.preventDefault();
-         this.props.handleUpdate(7);
-    }
-    clickEight(e){
-         e.preventDefault();
-         this.props.handleUpdate(8);
-    }
-    clickNine(e){
-         e.preventDefault();
-         this.props.handleUpdate(9);
-    }
+  //   constructor(props) {
+  //     super(props);
+  //     this.clickOne = this.clickOne.bind(this);
+  //     this.clickTwo = this.clickTwo.bind(this);
+  //     this.clickThree = this.clickThree.bind(this);
+  //     this.clickFour = this.clickFour.bind(this);
+  //     this.clickFive = this.clickFive.bind(this);
+  //     this.clickSix = this.clickSix.bind(this);
+  //     this.clickSeven = this.clickSeven.bind(this);
+  //     this.clickEight = this.clickEight.bind(this);
+  //     this.clickNine = this.clickNine.bind(this);
+  //   }
+  //   clickOne(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(1);
+  //   }
+  //   clickTwo(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(2);
+  //   }
+  //   clickThree(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(3);
+  //   }
+  //   clickFour(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(4);
+  //   }
+  //   clickFive(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(5);
+  //   }
+  //   clickSix(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(6);
+  //   }
+  //   clickSeven(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(7);
+  //   }
+  //   clickEight(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(8);
+  //   }
+  //   clickNine(e){
+  //        e.preventDefault();
+  //        this.props.handleUpdate(9);
+  //   }
     render(){
       return(
         <div>
           <table>
             <tr>
-              <td><button onClick={this.clickOne}>One</button></td>
-              <td><button onClick={this.clickTwo}>Two</button></td>
-              <td><button onClick={this.clickThree}>Three</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(1)}>One</button></td>
+
+              {/* here to pass the value from direct handler which is decleard in the parent class
+              then we need to do these thing
+              1. onClick={()=>this.props.handler(valuepassed)}
+              2. on refrence <Parent handler = {this.handler}
+              3. in handler :- handler=(value)=>{
+                // my code
+              } */}
+              
+              <td><button onClick={()=>this.props.handleUpdate(2)}>Two</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(3)}>Three</button></td>
             </tr>
             <tr>
-              <td><button onClick={this.clickFour}>Four</button></td>
-              <td><button onClick={this.clickFive}>Five</button></td>
-              <td><button onClick={this.clickSix}>Six</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(4)}>Four</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(5)}>Five</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(6)}>Six</button></td>
             </tr>
             <tr>
-              <td><button onClick={this.clickSeven}>Seven</button></td>
-              <td><button onClick={this.clickEight}>Eight</button></td>
-              <td><button onClick={this.clickNine}>Nine</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(7)}>Seven</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(8)}>Eight</button></td>
+              <td><button onClick={()=>this.props.handleUpdate(9)}>Nine</button></td>
             </tr>
           </table>
         </div>
